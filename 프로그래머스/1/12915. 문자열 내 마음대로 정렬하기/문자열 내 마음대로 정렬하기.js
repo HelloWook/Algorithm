1ec8c61b9d08e82854d6df9522ac0eb436f3bd5b
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-05-31 02:01:55 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.19 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,12 @@
+function solution(strings, n) {
+  var answer = [];
+  answer = strings.sort((a, b) => {
+    if (a[n] === b[n]) {
+      return a.localeCompare(b);
+    }
+    return a[n].localeCompare(b[n]);
+  });
+  return answer;
+}
+
+console.log(solution(["abce", "abcd", "cdx"], 2));
