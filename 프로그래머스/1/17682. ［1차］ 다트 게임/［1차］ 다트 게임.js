8a2887faff8656b70e9e6ac568228b204d2c2a66
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-06-30 04:33:32 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,43 @@
+function solution(dartResult) {
+  const results = [];
+  let number = 0;
+
+  for (let i = 0; i < dartResult.length; i++) {
+    if (!isNaN(dartResult[i])) {
+      if (dartResult[i] === "1" && dartResult[i + 1] === "0") {
+        number = 10;
+        i++;
+      } else {
+        number = parseInt(dartResult[i], 10);
+      }
+    } else {
+      let len = results.length;
+      switch (dartResult[i]) {
+        case "S":
+          number = Math.pow(number, 1);
+          results.push(number);
+          break;
+        case "D":
+          number = Math.pow(number, 2);
+          results.push(number);
+          break;
+        case "T":
+          number = Math.pow(number, 3);
+          results.push(number);
+          break;
+        case "*":
+          if (len > 0) results[len - 1] *= 2;
+          if (len > 1) results[len - 2] *= 2;
+          break;
+        case "#":
+          if (len > 0) results[len - 1] *= -1;
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  return results.reduce((acc, curr) => acc + curr, 0);
+}
+// 지피티 보고 함 미친넘 
